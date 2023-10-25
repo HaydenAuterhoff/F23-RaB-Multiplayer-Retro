30c3650baa1bbf6321d285c12db606869000afb3
HaydenAuterhoff <91818047+HaydenAuterhoff@users.noreply.github.com>
2023-10-24 23:32:14 -0400

    Finished the Roll-a-Ball tutorial

---
+++ VARLab Multiplayer (Roll-a-ball - Fusion)/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
+    private Rigidbody _rb;
+    private int _count;
+    private float _movementX;
+    private float _movementY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _count = 0;
+        _rb = GetComponent<Rigidbody>();
+
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        _movementX = movementVector.x;
+        _movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + _count.ToString();
+
+        if (_count >= 6)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(_movementX, 0.0f, _movementY);
+        _rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            _count++;
+            SetCountText();
+        }
+    }
+}
